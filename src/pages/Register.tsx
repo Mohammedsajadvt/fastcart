4294a7c01ff9b0d56fa7b1b13bd210c24d0c26da
Mohammed Sajad VT <Sajad.vt@kiebot.com>
2025-06-15 22:08:39 +0530

    Added Mui , Router

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,22 @@
+import { Box, Container, Paper, TextField, Typography } from "@mui/material";
 
 
 const Register = () => { 
     return (
-        <></>
+       <Container maxWidth="xs" sx={{ mt: 8 }}>
+        <Paper sx={{background:'#ffffff',borderRadius:2,p: 4}}>
+            <Typography variant="h5" align="center"  gutterBottom fontFamily="Inter" fontSize={30} fontWeight="bold">Create an Account</Typography>
+             <Box component="form"  noValidate>
+            <TextField
+            fullWidth
+            margin="normal"
+            label="Full Name"
+            name="name"
+            required
+          />
+             </Box>
+        </Paper>
+       </Container>
     );
 };
 
---
+++ src/theme/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    typography: {
+        fontFamily: `'Inter', sans-serif`,
+    },
+    palette:{
+        background:{
+            default: '#D9E1EC',
+        }
+    }
+});
+
+export default theme;
